e9646c9c2704ea308a0c731d378ec4e07207e81d
Kevin <k3v1n.k4md3m@gmail.com>
2025-02-21 16:08:04 +0100

    basic action for a todo app list can be stored in txt file

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,15 +1,38 @@
 
-todos= []
+
 while True:
     user_action = (input("Type add, edit, show, complete or exit: ")).strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
-            todos.append(todo)
-            todos.sort()
-        case 'show'|'display':
-            for  index, todo_item in enumerate(todos):
+
+            file = open("Files/todos.txt", "r")  # the file name todoo opened in write mode
+            todos = file.readlines()       # the inhalt ist ausgelesen und ist stored in the list named todos
+            file.close()
+
+
+
+            todos.append(new_todo + '\n')      # the new todoo item is appended to list read from the file todos
+            todos.sort(key=str.lower)
+
+            file = open("files/todos.txt", "w")  # a new file is created in write mode and the old one does not exist anymore becuase it is now overwritten
+            file.writelines(todos)         #the new list ist now written in the new created todos file with the new appended todoo item
+            file.close()
+
+        case 'show'|'display':#
+            file = open("files/todos.txt", 'r')
+            todos = file.readlines()
+            file.close()
+
+            new_todos = []
+ #           for todo_item in todos:
+ #               new_todo = todo_item.strip('\n')  # to remove the back slash by each item of the list
+ #               new_todos.append(new_todo)
+
+            new_todos = [todo_item.strip('\n') for todo_item in todos] # list comprehension to contract the for lop in one line
+
+            for  index, todo_item in enumerate(new_todos):
                 print(f"{index+1}-{todo_item.capitalize()}")
 
         case 'edit':
